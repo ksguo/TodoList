911c4c59e1c6e766c20108d75780a69e46eb1fac
ks_guo <kguo1@smail.uni-koeln.de>
2025-02-20 20:20:22 +0100

    add userdauft and noitemsview

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by spike on 20.02.25.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
